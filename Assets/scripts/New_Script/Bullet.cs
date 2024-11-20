b44ba951d8438205fabc162de59c4fb3ed09a06c
-Rd <rodrigo.maturanaa@estudiantes.arcos.cl>
2024-11-20 12:15:11 -0300

    Tomate

--- Assets/scripts/New_Script/Bullet.cs
+++ Assets/scripts/New_Script/Bullet.cs
@@ -2,10 +2,23 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    public float damage = 10f; 
+    public float damage = 10f; // Daño de la bala
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider other)
     {
+        // Comprobar si el objeto con el que ha colisionado es un enemigo
+        if (other.CompareTag("Enemy"))
+        {
+            // Obtener el componente del enemigo (suponiendo que tenga un script Enemy con un método TakeDamage)
+            Boss_Phase enemy = other.GetComponent<Boss_Phase>();
+
+            if (enemy != null)
+            {
+                enemy.TakeDamage(damage);  // Aplicar el daño al enemigo
+            }
+        }
+
+        // Destruir la bala cuando colisione con cualquier objeto
         Destroy(gameObject);
     }
 }
