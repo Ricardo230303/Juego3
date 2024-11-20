d416fe8d152028aaf7fe11e8b836f8737fef7678
-Rd <rodrigo.maturanaa@estudiantes.arcos.cl>
2024-11-20 14:03:13 -0300

    Menu

---
+++ Assets/scripts/New_Script/Range.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Range : MonoBehaviour
+{
+    [SerializeField] UnityEvent onPlayerEnter;
+    [SerializeField] UnityEvent onPlayerExit;
+    [SerializeField] UnityEvent onPlayerStay;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            onPlayerEnter?.Invoke();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            onPlayerExit?.Invoke();
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            onPlayerStay?.Invoke();
+        }
+    }
+}
